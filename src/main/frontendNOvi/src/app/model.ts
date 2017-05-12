04d3a08ca6c8895cfeda06b01eebfdcc3e767983
Miroslav Košpić <miroslavkospic@yahoo.com>
2017-05-12 19:04:47 +0200

    as

---
+++ src/main/frontendNOvi/src/app/model.ts
@@ -0,0 +1,17 @@
+export class Guest
+{
+
+  constructor( public id: number,
+               public first_name : string,
+               public last_name : string,
+               public email : string,
+               public password : string,
+               public confirmReg : boolean
+
+  ) {}
+}
+
+
+
+
+
