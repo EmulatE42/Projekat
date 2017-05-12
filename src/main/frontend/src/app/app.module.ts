622ee383ee18a1621af7d5521217374dd78cbd57
Miroslav Košpić <miroslavkospic@yahoo.com>
2017-05-12 19:23:30 +0200

    valjda ???

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProbaComponent } from './proba/proba.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProbaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/main/frontend/src/app/proba/proba.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {ProbaService} from '../proba.service';
+import {User} from '../model';
+@Component({
+  moduleId: module.id,
+  selector: 'app-proba',
+  templateUrl: './proba.component.html',
+  styleUrls: ['./proba.component.css'],
+  providers: [ProbaService]
+})
+export class ProbaComponent implements OnInit {
+
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  user: User;
+
+  constructor(private probaService: ProbaService) {
+    this.id = 5;
+  }
+
+  reg() {
+    this.probaService.regi(this.id, this.first_name, this.last_name, this.email, this.password).subscribe(data => {
+      this.user = data
+    }, error => alert(error));
+  }
+
+  ngOnInit() {
+  }
+
+}
