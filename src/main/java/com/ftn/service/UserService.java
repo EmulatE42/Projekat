c81922f8bb5d1348111430a0789b754a341c937a
momir <mkosticfcb95@gmail.com>
2017-05-13 10:33:42 +0200

    proba :)

--- src/main/java/com/ftn/service/UserService.java
+++ src/main/java/com/ftn/service/UserService.java
@@ -6,7 +6,7 @@ import com.ftn.domain.User;
  * Created by EmulatE on 10-May-17.
  */
 public interface UserService {
- // ovde su moje u rep su postojece
+ // ovde su moje u rep su postojece kom
     User findOne(Integer id);
     User save(User user) throws Exception;
 
